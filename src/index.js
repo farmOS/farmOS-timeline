a95c38cf8dd7fe23f9476cdf7abd75b7f4fd6d61
Paul Weidner <paul.weidner@gmail.com>
2024-07-15 12:18:45 -0700

    Make createInstance method

--- src/index.js
+++ src/index.js
@@ -1,59 +1,8 @@
-import {
-    SvelteGantt,
-    SvelteGanttTable,
-    MomentSvelteGanttDateAdapter
-} from 'svelte-gantt';
-import moment from 'moment';
+import createInstance from "./instance";
 
 class TimelineInstanceManager {
-
     create (target, options) {
-
-        const default_options = {
-            ganttTableModules: [SvelteGanttTable],
-            dateAdapter: new MomentSvelteGanttDateAdapter(moment),
-            tableHeaders: [{ title: target.dataset?.tableHeader, property: 'label', type: 'tree' }],
-            tableWidth: 240,
-            fitWidth: true,
-            from: Date.now()-86400,
-            to: Date.now(),
-            zoomLevels: [
-                {
-                    headers: [
-                        { unit: 'year', format: 'YYYY' },
-                        { unit: 'month', format: 'MMMM' }
-                    ],
-                    minWidth: 800,
-                    columnUnit: 'month',
-                    columnOffset: 1,
-                },
-                {
-                    headers: [
-                        { unit: 'month', format: 'MMMM YYYY' },
-                        { unit: 'day', format: 'D' },
-                    ],
-                    minWidth: 3200,
-                    columnUnit: 'day',
-                    columnOffset: 7,
-                },
-                {
-                    headers: [
-                        { unit: 'week', format: 'MMM YYYY' },
-                        { unit: 'day', format: 'D' }
-                    ],
-                    minWidth: 8000,
-                    columnUnit: 'hour',
-                    columnOffset: 12,
-                },
-            ],
-        }
-
-        return new SvelteGantt(
-            {
-                target: target,
-                props: {...default_options, ...options},
-            },
-        );
+        return createInstance({ target, options })
     }
 }
 
---
+++ src/instance.js
@@ -0,0 +1,57 @@
+import {MomentSvelteGanttDateAdapter, SvelteGantt, SvelteGanttTable} from "svelte-gantt";
+import moment from "moment/moment";
+
+const defaultZoomLevels = [
+  {
+    headers: [
+      { unit: 'year', format: 'YYYY' },
+      { unit: 'month', format: 'MMMM' }
+    ],
+    minWidth: 800,
+    columnUnit: 'month',
+    columnOffset: 1,
+  },
+  {
+    headers: [
+      { unit: 'month', format: 'MMMM YYYY' },
+      { unit: 'day', format: 'D' },
+    ],
+    minWidth: 3200,
+    columnUnit: 'day',
+    columnOffset: 7,
+  },
+  {
+    headers: [
+      { unit: 'week', format: 'MMM YYYY' },
+      { unit: 'day', format: 'D' }
+    ],
+    minWidth: 8000,
+    columnUnit: 'hour',
+    columnOffset: 12,
+  },
+];
+
+const createInstance = ({ target, options = {}}) => {
+  const default_options = {
+    props: {},
+  };
+  options = {...default_options, ...options};
+  const default_props = {
+    ganttTableModules: [SvelteGanttTable],
+    dateAdapter: new MomentSvelteGanttDateAdapter(moment),
+    tableHeaders: [{ title: target.dataset?.tableHeader, property: 'label', type: 'tree' }],
+    tableWidth: 240,
+    fitWidth: true,
+    from: Date.now()-86400,
+    to: Date.now(),
+    zoomLevels: defaultZoomLevels,
+  };
+  return {
+    timeline: new SvelteGantt({
+      target: target,
+      props: {...default_props, ...options.props},
+    }),
+  }
+}
+
+export default createInstance;
